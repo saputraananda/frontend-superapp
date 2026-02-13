e362015969688b6c97f733390f143a0be87eb871
saputraananda70@gmail.com <13042003Putra>
2026-02-13 09:59:26 +0700

    update : add MIME type

--- server.js
+++ server.js
@@ -9,7 +9,17 @@ const __dirname = path.dirname(__filename);
 const app = express();
 const distPath = path.join(__dirname, "dist");
 
-app.use(express.static(distPath));
+// Set MIME types
+express.static.mime.define({'application/javascript': ['js']});
+express.static.mime.define({'text/css': ['css']});
+
+app.use(express.static(distPath, {
+  setHeaders: (res, filePath) => {
+    if (filePath.endsWith('.js')) {
+      res.setHeader('Content-Type', 'application/javascript');
+    }
+  }
+}));
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(distPath, "index.html"));
